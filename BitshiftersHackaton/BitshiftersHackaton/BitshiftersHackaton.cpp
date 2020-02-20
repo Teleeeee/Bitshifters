549ee1f39d82ecd0add68bebe6f6dc64e3643d11
Levente Paplogo <lev9433@gmail.com>
2020-02-20 09:34:54 +0100

    initial project struct

---
+++ BitshiftersHackaton/BitshiftersHackaton/BitshiftersHackaton.cpp
@@ -0,0 +1,12 @@
+﻿// BitshiftersHackaton.cpp : Defines the entry point for the application.
+//
+
+#include "BitshiftersHackaton.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
