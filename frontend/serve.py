325bf83e45562282f99377a08a916b13bd502c66
balintkemeny <balintkemeny@gmail.com>
2020-02-21 00:33:08 +0100

    Created service endpoint on backend

--- frontend/serve.py
+++ frontend/serve.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask import render_template, send_from_directory
+from dotenv import load_dotenv
+import os
+import pysql_query
+import json
 
 app = Flask(__name__)
 
@@ -16,8 +20,17 @@ def index_page():
 def about_page():
 	return render_template('about.html')
 
+@app.route('/service')
+def service():
+	testData = pysql_query.queryDB()
+	return {
+		"time": str(testData[0].time),
+		"peopleCount": str(testData[0].cntPerson)
+	}
+
 if __name__ == '__main__':
-        app.run()
+	load_dotenv()
+	app.run()
 
 
 app.run(debug=True )
